c93014faae878cca4fef116679a43430df50a823
Ayoola731 <65588190+Ayoola731@users.noreply.github.com>
2022-03-04 19:05:21 -0600

    add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Jesus Loves Me, You and Everyone.! My comfort , My shelter'));
+app.get('/', (req, res) => res.send('Jesus Loves Me, You and Everyone.! My comfort , My shelter. Loruko Jesu, la n fowo soya, wipe ayo lawa yo fi lo odun yi ja!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
